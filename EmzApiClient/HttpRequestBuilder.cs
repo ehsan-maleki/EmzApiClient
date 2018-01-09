83035875100068c6e52bd6902270cce2cd7833a1
Ehsan Maleki Zoeram <ehsan.maleki@gmail.com>
2018-01-09 20:37:02 +0330

    Request timeout increased to 5 minutes and can be changed by user

--- EmzApiClient/HttpRequestBuilder.cs
+++ EmzApiClient/HttpRequestBuilder.cs
@@ -7,53 +7,58 @@ namespace EMZAM.DevTools.EmzApiClient
 {
     public class HttpRequestBuilder
     {
-        private HttpMethod method;
-        private string requestUri = "";
-        private HttpContent content;
-        private string bearerToken = "";
-        private string acceptHeader = "application/json";
-        private TimeSpan timeout = new TimeSpan(0, 0, 15);
-        private bool allowAutoRedirect;
+        private HttpMethod _method;
+        private string _requestUri = "";
+        private HttpContent _content;
+        private string _bearerToken = "";
+        private string _acceptHeader = "application/json";
+        private TimeSpan _timeout;
+        private bool _allowAutoRedirect;
+
+        public HttpRequestBuilder(TimeSpan timeout)
+        {
+            _timeout = timeout;
+        }
 
         public HttpRequestBuilder AddMethod(HttpMethod method)
         {
-            this.method = method;
+            this._method = method;
             return this;
         }
 
         public HttpRequestBuilder AddRequestUri(string requestUri)
         {
-            this.requestUri = requestUri;
+            this._requestUri = requestUri;
             return this;
         }
 
         public HttpRequestBuilder AddContent(HttpContent content)
         {
-            this.content = content;
+            this._content = content;
             return this;
         }
 
         public HttpRequestBuilder AddBearerToken(string bearerToken)
         {
-            this.bearerToken = bearerToken;
+            this._bearerToken = bearerToken;
             return this;
         }
 
         public HttpRequestBuilder AddAcceptHeader(string acceptHeader)
         {
-            this.acceptHeader = acceptHeader;
+            this._acceptHeader = acceptHeader;
             return this;
         }
 
         public HttpRequestBuilder AddTimeout(TimeSpan timeout)
         {
-            this.timeout = timeout;
+            this._timeout = timeout;
             return this;
         }
 
         public HttpRequestBuilder AddAllowAutoRedirect(bool allowAutoRedirect)
         {
-            this.allowAutoRedirect = allowAutoRedirect;
+            this._allowAutoRedirect = allowAutoRedirect;
             return this;
         }
 
@@ -65,42 +70,37 @@ namespace EMZAM.DevTools.EmzApiClient
             // Set up request
             var request = new HttpRequestMessage
             {
-                Method = method,
-                RequestUri = new Uri(requestUri)
+                Method = _method,
+                RequestUri = new Uri(_requestUri)
             };
 
-            if (content != null)
-                request.Content = content;
+            if (_content != null)
+                request.Content = _content;
 
-            if (!string.IsNullOrEmpty(bearerToken))
-                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", bearerToken);
+            if (!string.IsNullOrEmpty(_bearerToken))
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", _bearerToken);
 
             request.Headers.Accept.Clear();
-            if (!string.IsNullOrEmpty(acceptHeader))
-                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(acceptHeader));
+            if (!string.IsNullOrEmpty(_acceptHeader))
+                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(_acceptHeader));
 
             request.Headers.Add("User-Agent",
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36");
 
             // Setup client
-            using (var httpClientHandler = new HttpClientHandler {AllowAutoRedirect = allowAutoRedirect})
-            {
-                //httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
-                using (var client = new HttpClient(httpClientHandler) {Timeout = timeout})
-                {
+            using (var httpClientHandler = new HttpClientHandler {AllowAutoRedirect = _allowAutoRedirect})
+                using (var client = new HttpClient(httpClientHandler) {Timeout = _timeout})
                     return await client.SendAsync(request);
-                }
-            }
         }
 
         #region " Private "
 
         private void EnsureArguments()
         {
-            if (method == null)
+            if (_method == null)
                 throw new ArgumentNullException("Method");
 
-            if (string.IsNullOrEmpty(requestUri))
+            if (string.IsNullOrEmpty(_requestUri))
                 throw new ArgumentNullException("Request Uri");
         }
 
--- EmzApiClient/HttpRequestFactory.cs
+++ EmzApiClient/HttpRequestFactory.cs
@@ -1,19 +1,22 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace EMZAM.DevTools.EmzApiClient
 {
-     public static class HttpRequestFactory
+    public static class HttpRequestFactory
     {
+        public static readonly TimeSpan RequestTimeout = new TimeSpan(0, 5, 0);
+
         public static async Task<HttpResponseMessage> Get(string requestUri)
             => await Get(requestUri, "");
 
         public static async Task<HttpResponseMessage> Get(string requestUri, string bearerToken)
         {
-            var builder = new HttpRequestBuilder()
-                                .AddMethod(HttpMethod.Get)
-                                .AddRequestUri(requestUri)
-                                .AddBearerToken(bearerToken);
+            var builder = new HttpRequestBuilder(RequestTimeout)
+                .AddMethod(HttpMethod.Get)
+                .AddRequestUri(requestUri)
+                .AddBearerToken(bearerToken);
 
             return await builder.SendAsync();
         }
@@ -24,11 +27,11 @@ namespace EMZAM.DevTools.EmzApiClient
         public static async Task<HttpResponseMessage> Post(
             string requestUri, object value, string bearerToken)
         {
-            var builder = new HttpRequestBuilder()
-                                .AddMethod(HttpMethod.Post)
-                                .AddRequestUri(requestUri)
-                                .AddContent(new JsonContent(value))
-                                .AddBearerToken(bearerToken);
+            var builder = new HttpRequestBuilder(RequestTimeout)
+                .AddMethod(HttpMethod.Post)
+                .AddRequestUri(requestUri)
+                .AddContent(new JsonContent(value))
+                .AddBearerToken(bearerToken);
 
             return await builder.SendAsync();
         }
@@ -39,11 +42,11 @@ namespace EMZAM.DevTools.EmzApiClient
         public static async Task<HttpResponseMessage> Put(
             string requestUri, object value, string bearerToken)
         {
-            var builder = new HttpRequestBuilder()
-                                .AddMethod(HttpMethod.Put)
-                                .AddRequestUri(requestUri)
-                                .AddContent(new JsonContent(value))
-                                .AddBearerToken(bearerToken);
+            var builder = new HttpRequestBuilder(RequestTimeout)
+                .AddMethod(HttpMethod.Put)
+                .AddRequestUri(requestUri)
+                .AddContent(new JsonContent(value))
+                .AddBearerToken(bearerToken);
 
             return await builder.SendAsync();
         }
@@ -54,11 +57,11 @@ namespace EMZAM.DevTools.EmzApiClient
         public static async Task<HttpResponseMessage> Patch(
             string requestUri, object value, string bearerToken)
         {
-            var builder = new HttpRequestBuilder()
-                                .AddMethod(new HttpMethod("PATCH"))
-                                .AddRequestUri(requestUri)
-                                .AddContent(new PatchContent(value))
-                                .AddBearerToken(bearerToken);
+            var builder = new HttpRequestBuilder(RequestTimeout)
+                .AddMethod(new HttpMethod("PATCH"))
+                .AddRequestUri(requestUri)
+                .AddContent(new PatchContent(value))
+                .AddBearerToken(bearerToken);
 
             return await builder.SendAsync();
         }
@@ -69,10 +72,10 @@ namespace EMZAM.DevTools.EmzApiClient
         public static async Task<HttpResponseMessage> Delete(
             string requestUri, string bearerToken)
         {
-            var builder = new HttpRequestBuilder()
-                                .AddMethod(HttpMethod.Delete)
-                                .AddRequestUri(requestUri)
-                                .AddBearerToken(bearerToken);
+            var builder = new HttpRequestBuilder(RequestTimeout)
+                .AddMethod(HttpMethod.Delete)
+                .AddRequestUri(requestUri)
+                .AddBearerToken(bearerToken);
 
             return await builder.SendAsync();
         }
@@ -84,11 +87,11 @@ namespace EMZAM.DevTools.EmzApiClient
         public static async Task<HttpResponseMessage> PostFile(string requestUri,
             string filePath, string apiParamName, string bearerToken)
         {
-            var builder = new HttpRequestBuilder()
-                                .AddMethod(HttpMethod.Post)
-                                .AddRequestUri(requestUri)
-                                .AddContent(new FileContent(filePath, apiParamName))
-                                .AddBearerToken(bearerToken);
+            var builder = new HttpRequestBuilder(RequestTimeout)
+                .AddMethod(HttpMethod.Post)
+                .AddRequestUri(requestUri)
+                .AddContent(new FileContent(filePath, apiParamName))
+                .AddBearerToken(bearerToken);
 
             return await builder.SendAsync();
         }
