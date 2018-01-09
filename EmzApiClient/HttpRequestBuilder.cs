4bad5825cf847d4639c71fbb13f9a697a00c6dee
Ehsan Maleki Zoeram <ehsan.maleki@gmail.com>
2018-01-09 16:35:43 +0330

    Project Initiated...

--- EmzApiClient/HttpRequestBuilder.cs
+++ EmzApiClient/HttpRequestBuilder.cs
@@ -79,11 +79,18 @@ namespace EMZAM.DevTools.EmzApiClient
             if (!string.IsNullOrEmpty(acceptHeader))
                 request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(acceptHeader));
 
-            // Setup client
-            var handler = new HttpClientHandler {AllowAutoRedirect = allowAutoRedirect};
-            var client = new HttpClient(handler) {Timeout = timeout};
+            request.Headers.Add("User-Agent",
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36");
 
-            return await client.SendAsync(request);
+            // Setup client
+            using (var httpClientHandler = new HttpClientHandler {AllowAutoRedirect = allowAutoRedirect})
+            {
+                //httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+                using (var client = new HttpClient(httpClientHandler) {Timeout = timeout})
+                {
+                    return await client.SendAsync(request);
+                }
+            }
         }
 
         #region " Private "
